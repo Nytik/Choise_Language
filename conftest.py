400ec62013fece11801d34432967832aadacfa97
Nytik <sanek-butko@mail.ru>
2021-06-05 21:35:12 +0300

    добавил небольшой принт который сообщает что элемент есть

--- conftest.py
+++ conftest.py
@@ -1,39 +1,30 @@
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 import pytest
+import time
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='ru',
+    parser.addoption('--language', action='store', default=None,
                      help="Choose language: ru or es")
 
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
     browser = None
-
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
+
     if language == "ru":
-        print("\nstart RU LANGUAGE browser for test..")
+        print(f"\nstart {language} LANGUAGE browser for test..")
         browser = webdriver.Chrome(options=options)
     elif language == "es":
-        print("\nstart ES LANGUAGE browser for test..")
+        print(f"\nstart {language} LANGUAGE browser for test..")
         browser = webdriver.Chrome(options=options)
     else:
         raise pytest.UsageError("--language should be ru or es")
     yield browser
     print("\nquit browser..")
+    time.sleep(5)
     browser.quit()
 
-
-
-
-
-
-# link = "http://selenium1py.pythonanywhere.com/"
-#
-#
-# def test_guest_should_see_login_link(browser):
-#     browser.get(link)
-#     browser.find_element_by_css_selector("#login_link")
 
--- test_items.py
+++ test_items.py
@@ -1,6 +1,7 @@
 from selenium.common.exceptions import NoSuchElementException
+import time
 
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_have_button_basket(browser):
     browser.get(link)
