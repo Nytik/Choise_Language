149de05fafbc736db87052d7284b5aeab3b033b6
Nytik <sanek-butko@mail.ru>
2021-06-05 23:32:52 +0300

    КОСТЫНЬ len()

--- test_items.py
+++ test_items.py
@@ -5,10 +5,8 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_have_button_basket(browser):
     browser.get(link)
-    try:
-        browser.find_element_by_css_selector("[value='Добавить в корзину']")
-    except NoSuchElementException as error:
-        return error
-    return print('Элемент найден!')
+    q = len(browser.find_elements_by_css_selector("button.btn-primary"))
+    assert q > 0, 'Элемент не найден!'
+
 
 
