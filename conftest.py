b8778c7c6f46addce8a01b7bd4e75cd71564bf84
Nytik <sanek-butko@mail.ru>
2021-06-05 22:47:54 +0300

    нифига чет не получилось с тем циклом, поэтому просто копирнул результат в текстовую переменную z  и все заработало

--- conftest.py
+++ conftest.py
@@ -13,14 +13,12 @@ def browser(request):
     browser = None
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
-
-    for i in browser.find_elements_by_css_selector('select option'):
-        z = z + ' ' + i.get_attribute('value')
+    z = 'ar ca cs da de en-gb el es fi fr it ko nl pl pt pt-br ro ru sk uk zh-hans'
     if language in z:
         print(f"\nstart {language} LANGUAGE browser for test..")
         browser = webdriver.Chrome(options=options)
     else:
-        raise pytest.UsageError(f"--language should be in {z}")
+        raise pytest.UsageError(f"--language should be in '{z}'")
     yield browser
     print("\nquit browser..")
     time.sleep(5)
