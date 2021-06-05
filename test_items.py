0c15980cce31a1a3a3d3e46fc47a874aaf2ab430
Nytik <sanek-butko@mail.ru>
2021-06-05 21:06:12 +0300

    добавил тест который проверяет, что страница товара на сайте содержит кнопку добавления в корзину

---
+++ test_items.py
@@ -0,0 +1,13 @@
+from selenium.common.exceptions import NoSuchElementException
+
+link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+
+def test_have_button_basket(browser):
+    browser.get(link)
+    try:
+        browser.find_element_by_css_selector("[value='Добавить в корзину']")
+    except NoSuchElementException as error:
+        return error
+    return True
+
+
